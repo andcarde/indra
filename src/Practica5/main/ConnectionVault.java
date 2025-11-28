70a927c08c89c1836fb87be5efef631e571ec15e
dgamartin <dgamartin@indra.es>
2025-11-28 13:29:44 +0100

    Fix pom.xml

--- src/Practica5/main/ConnectionVault.java
+++ src/Practica5/main/ConnectionVault.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectionVault {
 
 	private static Connection instance;
-	private static final String URL  = "jdbc:mysql://localhost:3306/clinica";;
+	private static final String URL  = "jdbc:mysql://localhost:3306/clinica";
 	private static final String USER = "root";
 	private static final String PASS = "root";
 	
