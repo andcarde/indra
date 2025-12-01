25639bdd873853941a683c06640d8b603342acaa
dgamartin <dgamartin@indra.es>
2025-12-01 14:03:11 +0100

    NativeQuery

--- src/main/java/Practica6/Main.java
+++ src/main/java/Practica6/Main.java
@@ -1,26 +1,33 @@
 package Practica6;
 
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         ProductoDAO dao = new ProductoDAO();
 
-        // Crear producto
-        Producto p = new Producto();
-        p.setNombre("Laptop");
-        p.setPrecio(1200.0);
-        dao.create(p);
+//        // Crear producto
+//        Producto p = new Producto();
+//        p.setNombre("Laptop");
+//        p.setPrecio(1200.0);
+//        dao.create(p);
+//
+//        // Leer producto
+//        Producto producto = dao.find(p.getId());
+//        System.out.println("Producto: " + producto.getNombre());
+//
+//        // Actualizar producto
+//        producto.setPrecio(1300.0);
+//        dao.update(producto);
+//
+//        // Eliminar producto
+//        dao.delete(producto.getId());
 
-        // Leer producto
-        Producto producto = dao.find(p.getId());
-        System.out.println("Producto: " + producto.getNombre());
+        System.out.println("Buscando productos con 'Laptop':");
+        List<Producto> resultados = dao.findByName("Laptop");
+        resultados.forEach(prod -> System.out.println(prod.getId() + " - " + prod.getNombre() + " - " + prod.getPrecio()));
 
-        // Actualizar producto
-        producto.setPrecio(1300.0);
-        dao.update(producto);
-
-        // Eliminar producto
-        dao.delete(producto.getId());
     }
 }
 
