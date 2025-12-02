112b5842ee98d834c5e247c0d3b3fc6622611b53
dgamartin <dgamartin@indra.es>
2025-12-02 11:42:04 +0100

    Models

--- src/main/java/Practica7/model/Subject.java
+++ src/main/java/Practica7/model/Subject.java
@@ -4,19 +4,22 @@ package Practica7.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.List;
+
 @Entity
 @Data
-public class Student {
+public class Subject {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nombre;
-    private String apellido;
-    private String identificacion;
 
     @ManyToOne
-    @JoinColumn(name = "subject_id")
-    private Subject subject;
+    @JoinColumn(name = "teacher_id")
+    private Teacher teacher;
+
+    @OneToMany(mappedBy = "subject")
+    private List<Student> students;
 }
 
--- src/main/java/Practica7/model/Teacher.java
+++ src/main/java/Practica7/model/Teacher.java
@@ -4,19 +4,18 @@ package Practica7.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.List;
+
 @Entity
 @Data
-public class Student {
+public class Teacher {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nombre;
-    private String apellido;
-    private String identificacion;
 
-    @ManyToOne
-    @JoinColumn(name = "subject_id")
-    private Subject subject;
+    @OneToMany(mappedBy = "teacher")
+    private List<Subject> subjects;
 }
 
