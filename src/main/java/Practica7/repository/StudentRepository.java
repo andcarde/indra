153f7717f7b721d9f3f4b14836360574115b69af
andcarde <andrescardenalanton@gmail.com>
2025-12-02 13:35:49 +0100

    Práctica 7 eliminada.

--- src/main/java/Practica7/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package Practica7.repository;
-
-import Practica7.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student,Long> {
-}
--- src/main/java/Practica7/repository/SubjectRepository.java
+++
@@ -1,8 +0,0 @@
-package Practica7.repository;
-
-import Practica7.model.Subject;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubjectRepository extends JpaRepository<Subject,Long> {
-
-}
